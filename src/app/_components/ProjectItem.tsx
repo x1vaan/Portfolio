593c2993a3872824a0ceaa3404dd2902e45aa164
x1vaan <ivanx467@gmail.com>
2024-03-08 04:11:00 -0300

    more chips

--- src/app/_components/ProjectItem.tsx
+++ src/app/_components/ProjectItem.tsx
@@ -4,16 +4,16 @@ import { Chip } from "@nextui-org/react";
 
 export default function Projectitem(props: Project) {
   return (
-    <article className="w-[95%] md:max-w-[850px] h-auto flex flex-col md:flex-row justify-center items-start md:justify-start md:items-center relative mr-4 bg-[#09090B] rounded-lg gap-x-7">
+    <article className="w-[95%] md:max-w-[850px] h-auto flex flex-col md:flex-row justify-center items-start md:justify-start md:items-center relative mr-4 rounded-lg gap-7">
       <div className="w-full h-full relative shadow-lg">
         <Image src={props.image} alt="Project" sizes="100%" style={{ width: "100%", height: "100%" }} className="rounded-[0.5rem]" />
       </div>
       <div className="w-full h-full flex flex-col items-start gap-2 text-start relative">
         <h2 className="text-xl sm:text-2xl font-semibold text-[#D4D4D8]">{props.title}</h2>
-        <div className="w-full flex flex-row gap-x-2">
+        <div className="w-full flex flex-row gap-2 flex-wrap">
           {" "}
           {props.technologies.map((tech) => (
-            <Chip variant="flat" className={`${tech.class}`} startContent={<tech.svg width={20} height={20}/>}>{tech.name}</Chip>
+            <Chip variant="flat" className={`${tech.class} text-xs`} startContent={<tech.svg width={20} height={20}/>}>{tech.name}</Chip>
           ))}
         </div>
         <p className="text-slate-400 text-start text-pretty">
@@ -25,3 +25,5 @@ export default function Projectitem(props: Project) {
     </article>
   );
 }
+
+//classNames={{base: "bg-[#73bcf6]", content: "text-white"}} 
--- src/constants/project.ts
+++ src/constants/project.ts
@@ -1,9 +1,14 @@
 import NextJsSVG from "@/app/svgs/NextJs";
 import TailwindSVG from "../app/svgs/Tailwind";
 import TypeScriptSVG from "@/app/svgs/TypeScript";
+import PythonSVG from "@/app/svgs/Python";
+import NodeSVG from "@/app/svgs/Node";
+import PostgresSVG from "@/app/svgs/PostgreSQL";
 import { StaticImageData } from "next/image";
 import MewingDetector from "../../public/Mewing Detector.png";
 import { FC, SVGProps } from "react";
+import StrapiSVG from "@/app/svgs/Strapi";
+
 
 interface Technologies {
   name: string;
@@ -25,9 +30,9 @@ const projects: Project[] = [
       "Front para Aplicación que reconoce si la persona tiene “Mewing” (Los practicantes de esta técnica a menudo reportan una mandíbula más marcada y un rostro más estructurado).Y también backend con FastApi en Python para conectar con el engine y el modelo de reconocimiento de mewing.",
     technologies: [
       { name: "Next.js", svg: NextJsSVG, class: "bg-black text-white" },
-      { name: "TypeScript", svg: TypeScriptSVG, class: "bg-[#007acc] text-[#ffffff]" },
-      { name: "Tailwind CSS", svg: TailwindSVG, class: "bg-[#73bcf6] text-white" },
-      { name: "Python", svg: NextJsSVG, class: "bg-[#FFDE57] text-[#4584B6]" },
+      { name: "TypeScript", svg: TypeScriptSVG, class: "bg-[#fff] text-[#007acc]" },
+      { name: "Tailwind CSS", svg: TailwindSVG, class: "bg-[#003159] text-white" },
+      { name: "Python", svg: PythonSVG, class: "bg-[#FFDE57] text-[#4584B6]" },
     ],
     image: MewingDetector,
   },
@@ -36,10 +41,9 @@ const projects: Project[] = [
     description:
       "Front para Aplicación que reconoce si la persona tiene “Mewing” (Los practicantes de esta técnica a menudo reportan una mandíbula más marcada y un rostro más estructurado).Y también backend con FastApi en Python para conectar con el engine y el modelo de reconocimiento de mewing.",
     technologies: [
-      { name: "Next.js", svg: NextJsSVG, class: "bg-black text-white" },
-      { name: "TypeScript", svg: NextJsSVG, class: "bg-[#007acc] text-[#ffffff]" },
-      { name: "Tailwind CSS", svg: NextJsSVG, class: "bg-[#73bcf6] text-white" },
-      { name: "Python", svg: NextJsSVG, class: "bg-[#FFDE57] text-[#4584B6]" },
+      { name: "Node.js", svg: NodeSVG, class: "bg-[#303030] text-[#3c873a]" },
+      { name: "Strapi", svg: StrapiSVG, class: "bg-[#4945ff] text-[#ffffff]" },
+      { name: "PostgreSQL", svg: PostgresSVG, class: "bg-[#336791] text-white" },
     ],
     image: MewingDetector,
   },
