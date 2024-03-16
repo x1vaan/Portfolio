bb32583ef6050556ebe3400c9ad308fcb4f3da52
x1vaan <ivanx467@gmail.com>
2024-03-16 02:34:35 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={`w-full h-full absolute m-0 ${font.className} overflow-y-scroll bg-red-500`}>
-          <div className="absolute inset-0 -z-10 h-full items-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+        <body className={`w-full h-full absolute m-0 ${font.className} overflow-y-scroll [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]`}>
           {children}
         </body>
       </Providers>
